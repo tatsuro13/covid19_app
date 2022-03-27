4bb1c7b6c926f0c7f5f3833512083b14879834c7
Tatsuro Osuka <oscarwild13@gmail.com>
2022-03-27 10:38:03 +0900

    wip: Chart fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Cards from './features/covid/Cards/Cards';
+import Chart from './features/covid/Chart/Chart';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Cards />
+        <Chart />
       </header>
     </div>
   );
