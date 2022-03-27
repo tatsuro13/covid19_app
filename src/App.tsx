01d99e1557d6038e451d4b92e48eaf139b010611
Tatsuro Osuka <oscarwild13@gmail.com>
2022-03-27 14:14:37 +0900

    wip:SwitchCountry

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import Cards from './features/covid/Cards/Cards';
 import Chart from './features/covid/Chart/Chart';
 import PieChart from './features/covid/PieChart/PieChart';
+import SwitchCountry from './features/covid/SwitchCountry/SwitchCountry';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SwitchCountry />
         <Cards />
         <Chart />
         <PieChart />
