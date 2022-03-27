94c84c37c1a4ca0f4d4cb5aa73045631dc601132
Tatsuro Osuka <oscarwild13@gmail.com>
2022-03-27 13:39:56 +0900

    wip:PieChart

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Cards from './features/covid/Cards/Cards';
 import Chart from './features/covid/Chart/Chart';
+import PieChart from './features/covid/PieChart/PieChart';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <header className="App-header">
         <Cards />
         <Chart />
+        <PieChart />
       </header>
     </div>
   );
